769eb1a07f2329a6a3d38bb1eea6e68b3643d000
Kristaps Kozlovskis <160219564+codekristaps@users.noreply.github.com>
2024-12-01 11:07:31 +0200

    Added code

---
+++ AngularInsurancePortal/API/AngularInsurancePortal.API/AngularInsurancePortal.API/Models/Insurance.cs
@@ -0,0 +1,10 @@
+﻿namespace AngularInsurancePortal.API.Models
+{
+    public class Insurance
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}

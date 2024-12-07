68999f8eee9c32edb4175e9b31620f89d6fa38a2
Kristaps Kozlovskis <160219564+codekristaps@users.noreply.github.com>
2024-12-07 10:47:42 +0200

    Removed category component

--- AngularInsurancePortal/UI/AngularInsurancePortal/src/app/app.module.ts
+++ AngularInsurancePortal/UI/AngularInsurancePortal/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { CategoryComponent } from './category/category.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CategoryComponent
   ],
   imports: [
     BrowserModule,
